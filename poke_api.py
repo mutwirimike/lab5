a17b94f1b62e9a75ad0521bbe45a5f56428a8c6c
DennisDam439 <dend@flemingcollege.ca>
2024-07-12 16:37:04 -0400

    Update poke_api.py

--- poke_api.py
+++ poke_api.py
@@ -10,6 +10,7 @@ def main():
     # Test out the get_pokemon_info() function
     # Use breakpoints to view returned dictionary
     poke_info = get_pokemon_info("Rockruff")
+    print(poke_info)
     return
 
 def get_pokemon_info(pokemon_name):
@@ -22,14 +23,23 @@ def get_pokemon_info(pokemon_name):
         dict: Dictionary of Pokemon information, if successful. Otherwise None.
     """
     # TODO: Clean the Pokemon name parameter
+    pokemon_name = pokemon_name.lower().strip()
 
     # TODO: Build a clean URL and use it to send a GET request
+    url = f"{POKE_API_URL}{pokemon_name}/"
 
     # TODO: If the GET request was successful, convert the JSON-formatted message body text to a dictionary and return it
+    response =requests.get(url)
 
     # TODO: If the GET request failed, print the error reason and return None
+    if response.status_code== 200:
+        pokemon_data =response.json()
+        return pokemon_data
+    else:
+        print(f"failed to retrieve data: {response.status_code} - {response.reason}")
 
-    return
+
+    return None
 
 if __name__ == '__main__':
     main()
